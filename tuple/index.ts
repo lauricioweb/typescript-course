26949fbdb87fb6f646c35e8666fac8fea54e9ce4
lauricioweb <lauriciomalek@gmail.com>
2025-01-02 17:59:53 -0300

    tuples

--- tuple/index.ts
+++ tuple/index.ts
@@ -3,6 +3,8 @@
 const dados1:[string, number] = ["lauricio", 22];
 const dados:[string, number,string?] = ["lauricio", 22]; // parametro opcional
 
+const dados2:[string, number, ...string[]] = ["lau",22,"gosma","branca"]
+
 dados1[1] = 1000;
 //dados1.push("lauricioss") pode ser adicionado com o push porem nao é a forma correta
 
